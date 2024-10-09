261c1af58615ed8352534f00920fb11fcc407d5f
sathviksupadhya <108052592+sathviksupadhya@users.noreply.github.com>
2024-10-08 21:36:55 -0700

    Add files via upload
    uploaded airlines,flight_service,gateway

---
+++ AirlinesGateway/src/main/java/com/Airlinesapplication/AirlinesGateway/AirlinesGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.Airlinesapplication.AirlinesGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+
+public class AirlinesGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesGatewayApplication.class, args);
+	}
+
+}
